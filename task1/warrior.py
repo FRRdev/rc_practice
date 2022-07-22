dd8d834af0ca4fb90a5f95d360d7b03aeac533aa
Mikhail <mixail.critsyn@mail.ru>
2022-07-22 12:35:17 +0300

    refactoring decorators

--- task1/warrior.py
+++ task1/warrior.py
@@ -1,25 +1,9 @@
+import inspect
 import sys
 import functools
 
 
-def trace_action(cls):
-    """ Tracing all the actions that occur in warrior class
-    """
-
-    class Wrapper:
-        def __init__(self, *args):
-            self.wrapped = cls(*args)
-
-        def __getattr__(self, item):
-            if hasattr(self.wrapped, item) and \
-                    not isinstance(getattr(cls, item), property):
-                print(f'{cls.__name__} => {item}')
-            return getattr(self.wrapped, item)
-
-    return Wrapper
-
-
-def check_health(func):
+def __check_health(func):
     """ Life check after taking damage
     """
 
@@ -28,16 +12,46 @@ def check_health(func):
         health_left = func(*args, **kwargs)
         current_warrior_object = args[0]
         current_warrior_object_name = current_warrior_object.__class__.__name__
-        print(f'{current_warrior_object_name} has {current_warrior_object.health} hp\n')
         if health_left <= 0:
             print(f'{current_warrior_object_name} was killed!')
             sys.exit()
+        if health_left > current_warrior_object.max_health:
+            current_warrior_object.health = current_warrior_object.max_health
+        print(f'{current_warrior_object_name} has {current_warrior_object.health} hp\n')
 
     return wrapper
 
 
+def __trace_info(func):
+    """ Trace info about function
+    """
+
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        frame = inspect.currentframe()
+        current_warrior_name = frame.f_locals['self'].__class__.__name__
+        print(f'{current_warrior_name} => {func.__name__}')
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def class_decorator(cls):
+    """
+    The main decorator for the class that
+    adds a health check and information output
+    """
+    for parent in cls.mro():
+        for name, method in parent.__dict__.items():
+            if not name.startswith('__') and \
+                    not isinstance(getattr(cls, name), property):
+                setattr(cls, name, __check_health(__trace_info(method)))
+    return cls
+
+
 class BaseWarrior:
     def __init__(self, health):
+        self._max_health = health
         self._health = health
 
     def eat_apple(self):
@@ -48,10 +62,13 @@ class BaseWarrior:
         self._health += 10
         return self._health
 
-    @check_health
     def get_damaged(self):
         self._health -= 10
         return self._health
+
+    @property
+    def max_health(self):
+        return self._max_health
 
     @property
     def health(self):
@@ -62,18 +79,17 @@ class BaseWarrior:
         self._health = health
 
 
-@trace_action
+@class_decorator
 class Wizard(BaseWarrior):
     def __init__(self, health=100):
         super().__init__(health)
 
-    @check_health
     def get_damaged(self):
         self.health -= 20
         return self.health
 
 
-@trace_action
+@class_decorator
 class Paladin(BaseWarrior):
     def __init__(self, health=200):
         super().__init__(health)
@@ -83,15 +99,15 @@ class MyFieldManager:
     """ The Manager signaling the beginning and end of the battle
     """
 
-    def __init__(self, first_warrior, second_warrior):
+    def __init__(self, first_warrior: BaseWarrior, second_warrior: BaseWarrior):
         self.first_warrior = first_warrior
         self.second_warrior = second_warrior
 
     def __enter__(self):
         print(
             f'The battle between '
-            f'{self.first_warrior.wrapped.__class__.__name__} '
-            f'and {self.second_warrior.wrapped.__class__.__name__}\n'
+            f'{self.first_warrior.__class__.__name__} '
+            f'and {self.second_warrior.__class__.__name__}\n'
         )
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -102,10 +118,9 @@ if __name__ == '__main__':
     w = Wizard()
     p = Paladin()
     with MyFieldManager(w, p) as field:
-        p.get_damaged()
-        p.get_damaged()
         w.get_damaged()
-        p.eat_mushroom()
-        p.eat_mushroom()
-        p.eat_mushroom()
+        w.eat_mushroom()
+        w.eat_mushroom()
         p.get_damaged()
+        p.get_damaged()
+        p.eat_apple()
